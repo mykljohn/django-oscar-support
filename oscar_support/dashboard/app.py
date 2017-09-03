f0a45f9e7cc59a3b58c7f2ab5072e5876f6eb76b
SalahAdDin <alagunasalahaddin@live.com>
2017-09-03 19:37:50 +0300

    Update dashboard apps: firs steps.

--- oscar_support/dashboard/app.py
+++ oscar_support/dashboard/app.py
@@ -1,4 +1,5 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+from django.contrib.admin.views.decorators import staff_member_required
 
 from shortuuid import get_alphabet
 
@@ -16,21 +17,17 @@ class SupportDashboardApplication(Application):
     ticket_update_view = views.TicketUpdateView
 
     def get_urls(self):
-        urlpatterns = patterns(
-            '',
-            url(r'^$', self.ticket_list_view.as_view(), name='ticket-list'),
-            url(r'^ticket/create/$', self.ticket_create_view.as_view(),
+        urls = [
+            url(r'^$', staff_member_required(self.ticket_list_view.as_view()), name='ticket-list'),
+            url(r'^ticket/create/$', staff_member_required(self.ticket_create_view.as_view()),
                 name='ticket-create'),
             url(
                 r'^ticket/update/(?P<pk>[{0}]+)/$'.format(get_alphabet()),
-                self.ticket_update_view.as_view(),
+                staff_member_required(self.ticket_update_view.as_view()),
                 name='ticket-update'
             ),
-        )
-        return self.post_process_urls(urlpatterns)
-
-    def get_url_decorator(self, url_name):
-        return staff_member_required
+        ]
+        return self.post_process_urls(urls)
 
 
 application = SupportDashboardApplication()
