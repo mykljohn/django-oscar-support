5239a65610dfea461ba3d400f409223687aef020
SalahAdDin <alagunasalahaddin@live.com>
2017-09-17 01:27:52 +0300

    Update dashboard setup.

--- oscar_support/dashboard/app.py
+++ oscar_support/dashboard/app.py
@@ -3,14 +3,14 @@ from django.contrib.admin.views.decorators import staff_member_required
 
 from shortuuid import get_alphabet
 
-from oscar.core.application import Application
-from oscar.views.decorators import staff_member_required
+from oscar.core.application import DashboardApplication
 
 from . import views
 
 
-class SupportDashboardApplication(Application):
+class SupportDashboardApplication(DashboardApplication):
     name = 'support-dashboard'
+    default_permissions = ['is_staff', ]
 
     ticket_list_view = views.TicketListView
     ticket_create_view = views.TicketCreateView
@@ -18,12 +18,12 @@ class SupportDashboardApplication(Application):
 
     def get_urls(self):
         urls = [
-            url(r'^$', staff_member_required(self.ticket_list_view.as_view()), name='ticket-list'),
-            url(r'^ticket/create/$', staff_member_required(self.ticket_create_view.as_view()),
+            url(r'^$', self.ticket_list_view.as_view(), name='ticket-list'),
+            url(r'^ticket/create/$', self.ticket_create_view.as_view(),
                 name='ticket-create'),
             url(
                 r'^ticket/update/(?P<pk>[{0}]+)/$'.format(get_alphabet()),
-                staff_member_required(self.ticket_update_view.as_view()),
+                self.ticket_update_view.as_view(),
                 name='ticket-update'
             ),
         ]
